8233cc03db1b3d9597ddb5e263e24230d736eab1
Affiliatethis <chris@onecrookedtree.com>
2019-08-06 15:59:31 -0400

    Avatar pic is loading on the landing page.  I am confused how I fixed, but it is fixed.  Css conditions will be added to the image to size it and position it.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <div className="demo-big-content">
         <Layout>
-          <Header title="Title" scroll>
+          <Header className="header-color" title="Title" scroll>
             <Navigation>
               <Link to="./resume">Resume</Link>
               <Link to="./aboutme">About Me</Link>
@@ -27,11 +27,10 @@ class App extends Component {
           </Drawer>
           <Content>
             <div className="page-content" />
-            <Main/>
+            <Main />
           </Content>
-         
         </Layout>
-      </div>    
+      </div>
     );
   }
 }
--- src/Components/landingpage.js
+++ src/Components/landingpage.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+// import images from "./images";
+// import imageLoader from './images';
 
 class Landing extends Component {
   render() {
     return (
-      <div>
-        <h1>Landing Page</h1>
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            {/* add image import function */}
+            <img
+              src="https://icons-for-free.com/iconfiles/png/512/business+costume+male+man+office+user+icon-1320196264882354682.png"
+              alt="avatar"
+              className="avatar-img"
+            />
+          </Cell>
+        </Grid>
       </div>
     );
   }
