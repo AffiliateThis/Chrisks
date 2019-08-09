5b5b623ba350daa68f290409952dabafc508f747
Affiliatethis <chris@onecrookedtree.com>
2019-08-09 11:46:02 -0400

    more resume work,  Schools have been added. More updates to followwith experience.

--- src/Components/resume.js
+++ src/Components/resume.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Cell } from "react-mdl";
-import Iframe from "react-iframe";
+import Education from "./education";
 
 class Resume extends Component {
   render() {
@@ -8,13 +8,64 @@ class Resume extends Component {
       <div>
         <Grid>
           <Cell col={4}>
-          <div style={{ textAlign: "center" }}>
-            
-          </div>
-          <h2 style={{paddingTop: '2em'}}>Christian B. Kautz-Scanavy</h2>
+            <div style={{ textAlign: "center" }}>
+              <img
+                src="https://icons-for-free.com/iconfiles/png/512/business+costume+male+man+office+user+icon-1320196264882354682.png"
+                alt="avatar"
+                style={{ height: "200px" }}
+                className="resAvatar"
+              />
+            </div>
+
+            <h3 className="resName" style={{ paddingTop: "2em" }}>
+              Christian B. Kautz-Scanavy
+            </h3>
+            <h4 style={{ color: "grey" }}>Full Stack Developer</h4>
+            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
+            <p>
+              Team-oriented Web Developer with a background in digital marketing
+              and customer acquisition, with Full-Stack Certification from the
+              University of Richmond. Skilled in digital marketing tracking
+              platforms, CRM and SAS implementation/configurations. A proven
+              record of a superb work-ethic, stellar customer service and able
+              to successfully collaborate with team members. Excellent ability
+              in conveying complex technical information in an easy to
+              understand manner.
+            </p>
+            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
+            <h5>Phone</h5>
+            <p>(804) 357-1353</p>
+            <h5>Email</h5>
+            <p>chris.kautz@gmail.com</p>
+            <h5>Website</h5>
+            <p>chrisks.com</p>
           </Cell>
           <Cell className="resume-right-col" col={8}>
-            Right Side
+            <h2>Education</h2>
+            <Education
+              startYear={2019}
+              endYear={2019}
+              schoolName="The University of Richmond"
+              degree="Full Stack Web Development | Coding Certificate"
+              schoolDescription="An intensive boot camp program focused on gaining 
+              web development skills in HTML, CSS, JQuery, JavaScript, MySQL Databases, 
+              MongoDB, Express, NodeJS and React. 
+              "
+            />
+
+            <hr style={{borderTop: '3px solid #e22947'}}/>
+
+
+<Education
+              startYear={1996}
+              endYear={2000}
+              schoolName="Hampden-Sydney College"
+              degree="Bachelor of Arts in History and French"
+              schoolDescription="An intensive boot camp program focused on gaining 
+              web development skills in HTML, CSS, JQuery, JavaScript, MySQL Databases, 
+              MongoDB, Express, NodeJS and React. 
+              "
+            />
           </Cell>
         </Grid>
       </div>
