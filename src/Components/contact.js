de7a54e9d6c0268ed83d14fb855e0cc5e7e990cd
Affiliatethis <chris@onecrookedtree.com>
2019-08-08 20:53:17 -0400

    2 cells setup for the resume page, more updates to follow.

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Cell } from "react-mdl";
+import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
 
 class Contact extends Component {
   render() {
@@ -26,9 +26,28 @@ class Contact extends Component {
             </p>
           </Cell>
           <Cell col={6}>
-          <h2>Contact Me</h2>
-            <hr/>
-
+            <h2>Contact Me</h2>
+            <hr />
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: "30px", fontFamily: "Anton" }}
+                  >
+                    <i className="fa fa-phone-square" aria-hidden="true" />
+                    (804) 357-1353
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: "30px", fontFamily: "Anton" }}
+                  >
+                    <i className="fa fa-envelope" aria-hidden="true" />
+                    chris.kautz@gmail.com
+                  </ListItemContent>
+                </ListItem>
+              </List>
+            </div>
           </Cell>
         </Grid>
       </div>
--- src/Components/resume.js
+++ src/Components/resume.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+import Iframe from "react-iframe";
 
 class Resume extends Component {
   render() {
     return (
       <div>
-        <h1>Resume</h1>
+        <Grid>
+          <Cell col={4}>
+          <div style={{ textAlign: "center" }}>
+            
+          </div>
+          <h2 style={{paddingTop: '2em'}}>Christian B. Kautz-Scanavy</h2>
+          </Cell>
+          <Cell className="resume-right-col" col={8}>
+            Right Side
+          </Cell>
+        </Grid>
       </div>
     );
   }
