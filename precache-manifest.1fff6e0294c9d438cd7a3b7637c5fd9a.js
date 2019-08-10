82a0f37e126745c99bc9e35a8140e879ea2ef08a
Affiliatethis <chris@onecrookedtree.com>
2019-08-10 17:13:25 -0400

    Updates

---
+++ precache-manifest.1fff6e0294c9d438cd7a3b7637c5fd9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c026055b28310015ae087d4d62303f",
+    "url": "/chrisks/index.html"
+  },
+  {
+    "revision": "6dc7ea08f0919289a164",
+    "url": "/chrisks/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "2feb2b763e77e57e06fa",
+    "url": "/chrisks/static/css/main.b77aa074.chunk.css"
+  },
+  {
+    "revision": "6dc7ea08f0919289a164",
+    "url": "/chrisks/static/js/2.43d61a1c.chunk.js"
+  },
+  {
+    "revision": "2feb2b763e77e57e06fa",
+    "url": "/chrisks/static/js/main.6634d0e9.chunk.js"
+  },
+  {
+    "revision": "99fb7950cca21766ab9a",
+    "url": "/chrisks/static/js/runtime~main.5e65e25c.js"
+  }
+]);
